6b223b0fe96e13137fa60d4219f91d870c098994
braverokmc79 <braverokmc79@gmail.com>
2022-12-03 16:27:16 +0900

    10. 리렌더링 이해하기

--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -8,7 +8,13 @@ const ButtonWrapper = styled.div`
     margin-top:10px
 `;
 
-const LoginForm = () => {
+const FormWrapper = styled(Form)`
+    margin-top: 100px;
+    padding:10px;
+`;
+
+
+const LoginForm = ({ setIsLoggedIn }) => {
 
     const [id, setId] = useState('');
     const [password, setPassword] = useState('');
@@ -22,8 +28,13 @@ const LoginForm = () => {
     }, []);
 
 
+    const onSubmitForm = useCallback(() => {
+        console.log(id, password);
+        setIsLoggedIn(true);
+    }, [id, password])
+
     return (
-        <Form>
+        <FormWrapper onFinish={onSubmitForm}>
             <div>
                 <label htmlFor='user-id'>아이디</label>
                 <br />
@@ -45,7 +56,7 @@ const LoginForm = () => {
                 <Link href="/signup" ><Button>회원가입</Button></Link>
             </ButtonWrapper>
 
-        </Form >
+        </FormWrapper>
     );
 };
 
--- front/components/UserProfile.js
+++ front/components/UserProfile.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { Card, Avatar, Button } from 'antd';
 
-const UserProfile = () => {
+const UserProfile = ({ setIsLoggedIn }) => {
+    const onLogOut = useCallback(() => {
+        setIsLoggedIn(false);
+    }, []);
+
     return (
-        <div>
+        <Card
+            actions={[
+                <div key="twit">짹짹<br />0</div>,
+                <div key="followings">팔로잉<br />0</div>,
+                <div key="follower">팔로워<br />0</div>,
 
-        </div>
+            ]}
+        >
+
+            <Card.Meta
+                avatar={<Avatar>ZC</Avatar>}
+                title="Macaronics"
+            />
+            <Button onClick={onLogOut}>로그아웃</Button>
+        </Card>
     );
 };
 
