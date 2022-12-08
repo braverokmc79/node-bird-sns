b2cd3d832e979ae02319eca7665c1ae44d1e49a2
braverokmc79 <braverokmc79@gmail.com>
2022-12-08 15:04:46 +0900

    18. 더미데이터와 포스트폼 만들기

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,11 +1,18 @@
 
 import React from 'react';
 import AppLayout from './../components/AppLayout';
+import { useSelector } from 'react-redux';
+import PostCard from './../components/PostCard';
+import PostForm from './../components/PostForm';
 
 const Index = () => {
+    const { isLoggedIn } = useSelector((state) => state.user);
+    const { mainPosts } = useSelector((state) => state.post);
+
     return (
         <AppLayout>
-
+            {isLoggedIn && <PostForm />}
+            {mainPosts.map((post) => <PostCard key={post.id} post={post} />)}
         </AppLayout>
     );
 };
