bf897a370b19bfdee5f75d2e116dca87ca7379f6
braverokmc79 <braverokmc79@gmail.com>
2022-12-25 20:48:51 +0900

    40. 익스프레스로 라우팅하기

--- back/app.js
+++ back/app.js
@@ -1,19 +1,51 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+const { application } = require('express');
+const express = require('express');
+const app = express();
 
 
-    if (req.method === 'GET') {
+// app.get -> 가져오다
+// app.post -> 생성하다
+// app.put -> 전체 수정
+// app.delete -> 제거
+// app.patch -> 부분 수정
+// app.options -> 찔러보기
+// app.head -> 헤더만 가져오기(헤더 / 바디)
+//
 
-    } else if (req.method === 'POST') {
+app.post('/login');  
 
-    }
+app.get('/', (req, res) => {
+    res.send('hello express');
+});
 
-    res.end('Hello node');
+app.get('/api', (req, res) => {
+    res.send('hello api');
 });
 
 
-server.listen(3065, () => {
+app.get('/api/posts', (req, res) => {
+    res.json([
+        { id: 1, content: 'hello1' },
+        { id: 2, content: 'hello2' },
+        { id: 3, content: 'hello3' }
+    ])
+});
+
+
+
+app.post('/api/post', (req, res) => {
+    res.json({ id: 1, content: 'hello' })
+});
+
+
+app.delete('/api/post', (req, res) => {
+    res.json({ id: 1, content: 'hello' })
+});
+
+
+
+app.listen(3065, () => {
     console.log("서버 실행 중");
-})
+});
+
 
